a168aa47b7c77b8837dbe228fac283dd3681ecb7
VictorECarvalho <victorelitt@gmail.com>
2024-10-13 20:48:18 -0300

    footer

--- src/pages/footer.js
+++ src/pages/footer.js
@@ -1,27 +1,14 @@
 import styles from "../styles/footer.module.css";
-import { Press_Start_2P } from "next/font/google";
-
-const press_start_2p = Press_Start_2P({
-  weight: "400",
-  subsets: ["latin"],
-});
 
 export default function Footer() {
-  const instaLogo = "/img/instagram-logo.png";
-  const linkedLogo = "/img/linkedin-logo.png";
-  const pokeLogo = "/img/pokemon-logo.png";
-  const ideLogo = "/img/ide-logo.png";
-  const ideLogoPixelado = "/img/logo-pixelado-branco.png";
+  
+  const instaLogo = "/Assets/Instagram.png";
+  const linkedLogo = "/Assets/Linkedin.png";
+  const ideLogo = "/Assets/IDE.png";
 
   return (
-    <div
-      id="footer"
-      className={`${press_start_2p.className} ${styles.container}`}
-    >
-      <div className={`${styles.logos}`}>
-        <a href="https://www.pokemon.com/br/" target="_blank">
-          <img src={pokeLogo} className={styles.poke}></img>
-        </a>
+    <div className={styles.container}>
+      <div className={styles.logos}>
 
         <a href="https://www.instagram.com/ide_jr/" target="_blank">
           <img src={instaLogo} className={styles.insta}></img>
@@ -36,17 +23,16 @@ export default function Footer() {
         </a>
       </div>
 
-      <div className={`${styles.texto_footer}`}>
-        <p className={`${styles.copyright}`}>
-        株式会社ポケモン (The Pokemon Company) &copy; x IDE &copy; Todos os direitos
-          reservados.
+      <div>
+        <p className={styles.texto_footer}>
+        CineIDE © Todos os direitos reservados.
         </p>
-        <p className={`${styles.politicas}`}>
+        <p className={styles.texto_footer}>
           Termos de Uso | Política de Privacidade | Política de Cookies
         </p>
       </div>
       <div className={styles.img_canto}>
-        <img src={ideLogoPixelado}></img>
+        <img src={ideLogo}></img>
       </div>
     </div>
   );
