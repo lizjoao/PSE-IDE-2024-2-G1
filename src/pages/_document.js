a2c59ebf01a8c22f8732f9ec5b74f925cab9dde4
Fernando Longhi de Andrade <fernandolonghideandrade@gmail.com>
2024-10-14 21:01:18 -0300

    Remoção de arquivos inúteis

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -7,28 +7,12 @@ class MyDocument extends Document {
         <Head>
           {/* Netlify Widget ... spoliers com que trabalhamos?*/}
           <script async src="https://identity.netlify.com/v1/netlify-identity-widget.js" />
-          <link rel="preconnect" href="https://fonts.googleapis.com"/>
-          <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin/>
-          <link href="https://fonts.googleapis.com/css2?family=Press+Start+2P&display=swap" rel="stylesheet"/>
-          <link rel="shortcut icon" type="imagex/png" href="/img/pokemon-logo.png"></link>
-          <title>Site PS 2023/2</title>
+          <link rel="shortcut icon" type="imagex/png" href="/Assets/Cat_Skull.png"></link>
+          <title>Cine IDE</title>
         </Head>
         <body>
           <Main />
           <NextScript />
-          {/* spoliers com que trabalhamos?*/}
-          <script dangerouslySetInnerHTML={{
-            __html: `
-              if (window.netlifyIdentity) {
-                window.netlifyIdentity.on("init", user => {
-                  if (!user) {
-                    window.netlifyIdentity.on("login", () => {
-                      document.location.href = "/admin/";
-                    });
-                  }
-                });
-              }
-          `}}/>
         </body>
       </Html>
     )
